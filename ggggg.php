917a078fde189ccf0c5a986846d4499d7b23ddd7
Md Firoz Ahmed <82923134+FirozAhmed01766762474@users.noreply.github.com>
2022-06-08 14:25:06 +0600

    Add files via upload
    Buying House project

---
+++ ggggg.php
@@ -0,0 +1,13 @@
+<?php
+$val=5;
+function
+for($i=$val;$i<=val;$i--)
+{
+    for($j=1;$i<=$i;$j++)
+    {
+        echo "$j"
+    }
+    echo "<br>"
+     
+}
+?>
---
+++ upper triangular.php
@@ -0,0 +1,12 @@
+<?php
+$val=5;
+for($i=1;$i<=val;$i--)
+{
+    for($j=1;$i<=$i;$j++)
+    {
+        echo "$j"
+    }
+    echo "<br>"
+     
+}
+?>
